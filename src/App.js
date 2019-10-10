428c7ba1cd07beced6ef874ba1fba528d693f7f0
vuvantienhd96 <congtuocxauxa000@gmail.com>
2019-10-11 00:17:43 +0700

    jst-06: add router and link and notfount for reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import { Provider } from './Context';
 
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact2';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   
   return (
     <Provider>
+      <Router>
       <div className="App">
         <Header branName="Contact Manager"/>
         <div className="container">
-          <AddContact />
-          <Contacts />
+          <Switch>
+            <Route exact path="/" component={Contacts}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contact/add" component={AddContact}/>
+            <Route component={NotFound}/>
+          </Switch>
         </div>
       </div>
+      </Router>
     </Provider>
   );
 }
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -12,6 +12,9 @@ class Contacts extends Component {
                     const { Contacts } = value;
                     return (
                         <React.Fragment>
+                            <h1 className="display-4 mb-2">
+                                <span className="text-danger">Contact</span> List
+                            </h1>
                             {Contacts.map(contact =>
                                 <Contact key={contact.id} contact={contact} 
                                     />
